8cb0dac683f4847ac9a35c7bd34a99af2c977ddc
hendra <hendraa3005@gmail.com>
2020-01-12 14:03:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mynotesapp/adapter/NoteAdapter.java
@@ -0,0 +1,79 @@
+package com.example.mynotesapp.adapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mynotesapp.CustomOnItemClickListener;
+import com.example.mynotesapp.NoteAddUpdateActivity;
+import com.example.mynotesapp.R;
+import com.example.mynotesapp.entity.Note;
+
+import java.util.ArrayList;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+    private ArrayList<Note> listNotes = new ArrayList<>();
+    private Activity activity;
+
+    public NoteAdapter(Activity activity) {
+        this.activity = activity;
+    }
+
+    public ArrayList <Note> getListNotes() {
+        return listNotes;
+    }
+    public void setListNotes(ArrayList<Note>listNotes){
+        this.listNotes.clear();
+        this.listNotes.addAll(listNotes);
+        notifyDataSetChanged();
+    }
+
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_note,parent,false);
+        return new NoteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        holder.tvTitle.setText(listNotes.get(position).getTitle());
+        holder.tvDesc.setText(listNotes.get(position).getDescription());
+        holder.tvDate.setText(listNotes.get(position).getDate());
+        holder.cvNote.setOnClickListener(new CustomOnItemClickListener(position, new CustomOnItemClickListener.OnItemClickCallback(){
+            @Override
+            public void onItemClicked(View view, int position) {
+                Intent intent = new Intent(activity, NoteAddUpdateActivity.class);
+                intent.putExtra(NoteAddUpdateActivity.EXTRA_POSITION, position);
+                intent.putExtra(NoteAddUpdateActivity.EXTRA_NOTE, listNotes.get(position));
+                activity.startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_UPDATE);
+            }
+        }));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listNotes.size();
+    }
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle,tvDesc,tvDate;
+        CardView cvNote;
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tv_item_title);
+            tvDesc = itemView.findViewById(R.id.tv_item_description);
+            tvDate = itemView.findViewById(R.id.tv_item_date);
+            cvNote = itemView.findViewById(R.id.cv_item_note);
+        }
+    }
+}
